17c6d9b85539fb5ca9b909dbb3887dd243a3f471
olawale-kareem <olawale.kareem@decagon.dev>
2021-08-17 12:14:33 +0100

    postgress password modified

--- polls/polls/settings.py
+++ polls/polls/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'polls_db',
         'USER': 'mac',
-        'PASSWORD': None,
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
