3f46cc686914db77a0649ca2c8569adb88b3e874
olawale-kareem <olawale.kareem@decagon.dev>
2021-08-18 10:56:25 +0100

    corrected test

--- polls/polls_app/tests.py
+++ polls/polls_app/tests.py
@@ -34,7 +34,7 @@ def create_question(question_text, days):
 class QuestionIndexViewTests(TestCase):
 
     def test_no_questions(self):
-        response = self.client.get(reverse('poll_app:index'))
+        response = self.client.get(reverse('polls_app:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
@@ -42,6 +42,6 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question(self):
         create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse('poll_app:index'))
+        response = self.client.get(reverse('polls_app:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
